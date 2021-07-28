30b33f8e0a4b922f4eb7ae8e6b45465bac0549b4
Carlo Carbonell <carlo1678@hotmail.com>
2021-07-28 10:27:28 -0400

    couple changes

--- DOM/script.js
+++ DOM/script.js
@@ -1,15 +1,1 @@
-let callback = (num) => {
-  if (num % 2 == 0) {
-    console.log("the number is even");
-  } else if (num % 1 == 0) {
-    console.log("the number is odd");
-  } else if (typeof num != "number") {
-    console.log("what is this?");
-  }
-};
 
-callback(10);
-// for (let num = 0; num <= 10; num++) {
-//   callback(num);
-//   console.log(num);
-// }
